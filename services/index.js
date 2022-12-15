381918a2d62ccf5cbde945647676008cd1365c55
evewolfs <53898640+evewolfs@users.noreply.github.com>
2022-12-15 17:50:07 +0100

    added categories

---
+++ services/index.js
@@ -0,0 +1,47 @@
+import { request, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+            content {
+              text
+            }
+          }
+        }
+      }
+    }
+    
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.categories;
+};
