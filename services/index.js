be83b662204785a54207839f37f1115fe9de66bd
evewolfs <53898640+evewolfs@users.noreply.github.com>
2022-12-15 18:54:56 +0100

    fixed categories

--- services/index.js
+++ services/index.js
@@ -45,3 +45,35 @@ export const getCategories = async () => {
 
   return result.categories;
 };
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+            content {
+              text
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
