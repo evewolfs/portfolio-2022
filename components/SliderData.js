d0aa7f13ab1c9800c67b2687bd7d31b0f48d5760
evewolfs <53898640+evewolfs@users.noreply.github.com>
2023-01-06 13:58:44 +0100

    minor change

--- components/SliderData.js
+++ components/SliderData.js
@@ -1,8 +1,7 @@
 export const SliderData = [
     {text: 'designer & developer'},
-    {text: "craft-lover & tinkerer" },
-    {text: 'mediator & INFP'},
-    {text: "cyclist & swimmer" },
-    {text: 'brussels-born & based'},
+    {text: "mediator & tinkerer"},
     {text: "90's kid & libra rising" },
+    {text: 'brussels-based & brewed'},
+    {text: 'cyclist & potato coach'},
 ]
