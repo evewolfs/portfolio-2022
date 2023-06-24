7f35bc122628d737fedfbf0a883a550e994dffcb
evewolfs <53898640+evewolfs@users.noreply.github.com>
2023-06-24 17:54:41 +0200

    GA tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "../components";
-
+import Script from 'next/script'
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 import "../styles/switcher.scss";
@@ -8,10 +8,26 @@ import "../styles/switcher.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    
+    <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-ZEXW9RRLVN"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZEXW9RRLVN', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   );
 }
 
