bfb3daa975703aa23f93be133a4e06f49ce9f8cb
evewolfs <53898640+evewolfs@users.noreply.github.com>
2024-10-28 15:18:47 +0100

    debug

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isGithubPages = process.env.GITHUB_ACTIONS || false;
 const repo = 'portfolio-2022'; // Replace with your GitHub repository name
 
 module.exports = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   assetPrefix: isGithubPages ? `/${repo}/` : '',
   basePath: isGithubPages ? `/${repo}` : '',
 };
